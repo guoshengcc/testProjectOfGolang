4d1bfe6443dcffe378057364a545b27db789c722
guoshenga <guoshenga@digitalchina.com>
2021-02-23 11:51:32 +0800

    add function AddHeadMsg

--- cmd/addhead.go
+++ cmd/addhead.go
@@ -3,6 +3,11 @@
 **/
 package cmd
 
+import (
+	"fmt"
+	"os"
+)
+
 // FileType 文件类型
 type FileType uint32
 
@@ -23,11 +28,48 @@ type FileEntity struct {
 }
 
 // AddHeadMsg 向文件头添加信息
+// 只针对UTF-8项目
 func AddHeadMsg(fe FileEntity, msg string) error {
-	// TODO
-	// 参考：https://blog.csdn.net/sillyyijia/article/details/105425503
-	// 判断文件是否存在
+	// 读取目标文件
+	fileObj, err := os.Open(fe.filePath) // ./hello.txt
+	if err != nil {
+		fmt.Printf("open file filed ,err :%v", err)
+		return err
+	}
 
-	// 向文件开始部分写入一段话，不进行覆盖
-	return nil
+	// 创建或打开缓存文件
+	tempObj, err := os.OpenFile("./temp.temp", os.O_WRONLY|os.O_TRUNC|os.O_CREATE, 0644)
+	if err != nil {
+		fmt.Printf("open file filed,err:%v", err)
+		return err
+	}
+	_, err2 := tempObj.Write([]byte(msg)) // 向文件末尾写入，此时的文件末尾实为文件开头
+	if err2 != nil {
+		fmt.Printf("write file filed ,err:%v", err)
+		return err2
+	}
+	var temp [128]byte
+	for {
+		n, err := fileObj.Read(temp[:])
+		if err != nil {
+			fmt.Printf("read file filed ,err :%v", err)
+			return err
+		}
+		_, err1 := tempObj.Write(temp[:n])
+		if err1 != nil {
+			fmt.Printf("write file filed ,err:%v", err)
+			return err1
+		}
+		if n < 128 {
+			fileObj.Close()
+			tempObj.Close()
+			err := os.Rename("./temp.temp", fe.filePath)
+			if err != nil {
+				fmt.Printf("rename file filed,err:%v", err)
+				return err
+			}
+			fmt.Println("finished!!")
+			return nil
+		}
+	}
 }
--- cmd/addhead_test.go
+++ cmd/addhead_test.go
@@ -5,9 +5,25 @@ import "testing"
 func TestAddHeadMsg(t *testing.T) {
 	// TODO
 	fe := FileEntity{
-		filePath: "/tmp/test.go",
+		filePath: "C:/tmp/tidal.go",
 		fileType: GO,
 	}
+	msg := `// Copyright 2019 PingCAP, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-	AddHeadMsg(fe, "juse test")
+`
+	err := AddHeadMsg(fe, msg)
+	if err != nil {
+		t.Error(err)
+	}
 }
