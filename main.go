acc26ef1b6686eae22efc985090066835dbb66df
guoshenga <guoshenga@digitalchina.com>
2021-02-24 17:33:10 +0800

    add cmd param

--- main.go
+++ main.go
@@ -1,13 +1,19 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"testproject/cmd"
 )
 
 func main() {
-	// fmt.Println("Hi")
-	err := cmd.Run("C:\\GitHub\\gomock", "./HeadContent.xml")
+	var targetDirPath, configFilePath string
+	flag.StringVar(&targetDirPath, "t", "", "target dir path")
+	flag.StringVar(&configFilePath, "c", "./HeadContent.xml", "the haed content config XML file path,Default is './HeadContent.xml'")
+	flag.Parse()
+	fmt.Println("接收的外部参数信息：")
+	fmt.Printf("targetDirPath:\t%s\nconfigFilePath:\t%s", targetDirPath, configFilePath)
+	err := cmd.Run(targetDirPath, configFilePath)
 	if err != nil {
 		fmt.Println(err)
 	} else {
